73c3c7656424a99de11c0b4ae1b3bbfcf905e8df
CarterStefan <ssttefancarter@gmail.com>
2020-07-17 13:18:27 +0000

    7 day weather description showing on page

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,10 +1,10 @@
-function getData(cb) {
+const baseURL =
+  "https://api.weatherbit.io/v2.0/forecast/daily?postal_code=ST162RP&days=7&key=8adc9c07736e467cb1e8ef7497ddc0f8";
+
+function getWeather(cb) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open(
-    "GET",
-    "https://api.weatherbit.io/v2.0/forecast/daily?postal_code=ST162RP&days=7&key=8adc9c07736e467cb1e8ef7497ddc0f8"
-  );
+  xhr.open("GET", baseURL);
 
   xhr.send();
 
@@ -15,8 +15,19 @@ function getData(cb) {
   };
 }
 
-function printDataToConsole(data){
-    console.log(data);
-}
 
-getData(printDataToConsole);
+
+function writeToDocument() {
+  let el = document.getElementById("data");
+  el.innerHTML = "";
+  getWeather(function (weather) {
+    weather = weather.data;
+
+    weather.forEach(function(item) {
+        Object.keys(item).forEach(function(key){
+            console.log(key);
+        })
+      el.innerHTML += "<p>" + item.weather.description + "</p>";
+    });
+  });
+}
