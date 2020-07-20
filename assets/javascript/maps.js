629f7a36f4d92b547c1f4991384a558bd8de4bb2
CarterStefan <ssttefancarter@gmail.com>
2020-07-20 15:42:52 +0000

    all markers now drop on click and show respective icons

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -1,4 +1,13 @@
-// example data (marker and infowindow data)
+// INITIALISE GOOGLE MAP
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: { lat: 52.8072076, lng: -2.1173127 },
+  });
+  infowindow = new google.maps.InfoWindow();
+}
+
+// RESTAURANTS
 var restaurants = [
   {
     position: { lat: 52.807589, lng: -2.117275 },
@@ -32,13 +41,83 @@ var restaurants = [
   },
 ];
 
+// ATTRACTIONS 
+
+var attractions = [
+  {
+    position: { lat: 52.806302, lng: -2.117101 },
+    image: { url: "assets/img/see.png" },
+    content: { html: "<h1>Ancient High House</h1><p>See Website</p>" },
+  },
+  {
+    position: { lat: 52.798038, lng: -2.147365 },
+    image: { url: "assets/img/see.png" },
+    content: { html: "<h1>Stafford Castle</h1><p>See Website</p>" },
+  },
+  {
+    position: { lat: 52.804004, lng: -2.120334 },
+    image: { url: "assets/img/see.png" },
+    content: { html: "<h1>Victoria Park</h1><p>See Webiste</p>" },
+  },
+  {
+    position: { lat: 52.820263, lng: -2.145788 },
+    image: { url: "assets/img/see.png" },
+    content: { html: "<h1>Doxey Marshes</h1><p>See Website</p>" },
+  },
+  {
+    position: { lat: 52.807201, lng: -2.11675 },
+    image: { url: "assets/img/see.png" },
+    content: { html: "<h1>Shire Hall Gallery</h1><p>See Website</p>" },
+  },
+  {
+    position: { lat: 52.807243, lng: -2.115737 },
+    image: { url: "assets/img/see.png" },
+    content: { html: "<h1>Gatehouse Theatre</h1><p>See Website</p>" },
+  },
+];
+
+// HOTELS
+
+var hotels = [
+  {
+    position: { lat: 52.806075, lng: -2.116975 },
+    image: { url: "assets/img/hotel.png" },
+    content: { html: "<h1>The Swan Hotel</h1><p>Book A Room</p>" },
+  },
+  {
+    position: { lat: 52.808092, lng: -2.116838 },
+    image: { url: "assets/img/hotel.png" },
+    content: { html: "<h1>The Vine</h1><p>Book A Room</p>" },
+  },
+  {
+    position: { lat: 52.822411, lng: -2.134624 },
+    image: { url: "assets/img/hotel.png" },
+    content: { html: "<h1>Tillington Hall</h1><p>Book A Room</p>" },
+  },
+  {
+    position: { lat: 52.797033, lng: -2.100978 },
+    image: { url: "assets/img/hotel.png" },
+    content: { html: "<h1>Spittal Brook</h1><p>Book A Room</p>" },
+  },
+  {
+    position: { lat: 52.799386, lng: -2.104048 },
+    image: { url: "assets/img/hotel.png" },
+    content: { html: "<h1>Abbey Hotel</h1><p>Book A Room</p>" },
+  },
+  {
+    position: { lat: 52.800163, lng: -2.106002 },
+    image: { url: "assets/img/hotel.png" },
+    content: { html: "<h1>Cameron Lodge</h1><p>Book A Room</p>" },
+  },
+];
+
 // Declare Global Variable
 var image;
 var markers = [];
 var map;
 var infowindow;
 
-// Initialization Google Map
+// INITIALISE GOOGLE MAP
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
@@ -47,11 +126,11 @@ function initMap() {
   infowindow = new google.maps.InfoWindow();
 }
 
-// Function Read data to Pre-Plot Marker
-function drop() {
+// FUNCTION TO ADD INFO TO RESTAURANT MARKERS
+function dropRestaurants() {
   clearMarkers();
   for (var i = 0; i < restaurants.length; i++) {
-    addMarkerWithTimeout(
+    addRestaurantMarkersWithTimeout(
       restaurants[i].position,
       restaurants[i].image,
       i * 200,
@@ -60,8 +139,34 @@ function drop() {
   }
 }
 
-// Function Plot Marker with animation
-function addMarkerWithTimeout(position, image, timeout, content) {
+// FUNCTION TO ADD INFO TO ATTRACTION MARKERS
+function dropAttractions() {
+  clearMarkers();
+  for (var i = 0; i < restaurants.length; i++) {
+    addAttractionsMarkersWithTimeout(
+      attractions[i].position,
+      attractions[i].image,
+      i * 200,
+      attractions[i].content.html
+    );
+  }
+}
+
+// FUNCTION TO ADD INFO TO HOTEL MARKERS
+function dropHotels() {
+  clearMarkers();
+  for (var i = 0; i < restaurants.length; i++) {
+    addHotelsMarkersWithTimeout(
+      hotels[i].position,
+      hotels[i].image,
+      i * 200,
+      hotels[i].content.html
+    );
+  }
+}
+
+// RESTAURANT FUNCTION
+function addRestaurantMarkersWithTimeout(position, image, timeout, content) {
   var marker;
   console.log(content);
   window.setTimeout(function () {
@@ -80,7 +185,47 @@ function addMarkerWithTimeout(position, image, timeout, content) {
   }, timeout);
 }
 
-// Function Clear All Marker in map
+// ATTRACTIONS FUNCTION
+function addAttractionsMarkersWithTimeout(position, image, timeout, content) {
+  var marker;
+  console.log(content);
+  window.setTimeout(function () {
+    marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      icon: image,
+      animation: google.maps.Animation.DROP,
+    });
+    google.maps.event.addListener(marker, "click", function () {
+      // Add listener on click to marker
+      infowindow.open(map, marker); // Show Infowindow
+      infowindow.setContent(content); // Set Content to Infowindow
+    });
+    markers.push(marker);
+  }, timeout);
+}
+
+// ATTRACTIONS FUNCTION
+function addHotelsMarkersWithTimeout(position, image, timeout, content) {
+  var marker;
+  console.log(content);
+  window.setTimeout(function () {
+    marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      icon: image,
+      animation: google.maps.Animation.DROP,
+    });
+    google.maps.event.addListener(marker, "click", function () {
+      // Add listener on click to marker
+      infowindow.open(map, marker); // Show Infowindow
+      infowindow.setContent(content); // Set Content to Infowindow
+    });
+    markers.push(marker);
+  }, timeout);
+}
+
+// FCLEAR ALL MARKERS FUNCTION
 function clearMarkers() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
