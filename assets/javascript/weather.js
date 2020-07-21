897823ba4938ace4d91764f80bedf7b328fb3111
CarterStefan <ssttefancarter@gmail.com>
2020-07-21 12:20:16 +0000

    added if statements with messages for all weather codes

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -28,10 +28,20 @@ function writeToDocument() {
     weather.forEach(function (item) {
       Object.keys(item).forEach(function (key) {});
 
-      if (item.weather.code == 803) {
-        msg = "you may need sunglasses";
-      } else if (item.weather.code == 804) {
-        msg = "no sun today";
+      if (item.weather.code >=200 && item.weather.code <=233) {
+        msg = "be careful out there!";
+      } else if (item.weather.code >=300 && item.weather.code <=522) {
+        msg = "you'll need a coat!";
+      } else if (item.weather.code >=600 && item.weather.code <=623) {
+        msg = "wrap up warm!"; 
+      } else if (item.weather.code >=700 && item.weather.code <=751) {
+        msg = "it may be hard to see today!"; 
+      } else if (item.weather.code == 800) {
+        msg = "bring your sunglasses!"; 
+      } else if (item.weather.code >=801 && item.weather.code <=803) {
+        msg = "you may need sunglasses!"; 
+      } else if (item.weather.code ==804) {
+        msg = "bit grey today!"; 
       } else {
         msg = "Enjoy Your Day";
       }
